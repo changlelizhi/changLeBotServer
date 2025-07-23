44809d4a2d4d4e167773af59a06feb134deada28
长乐 <222224158+changlelizhi@users.noreply.github.com>
2025-07-23 19:56:56 +0800

    创建项目

---
+++ src/main/java/com/changle/ChangLeBotServerApplication.java
@@ -0,0 +1,13 @@
+package com.changle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangLeBotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangLeBotServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/changle/ChangLeBotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangLeBotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
